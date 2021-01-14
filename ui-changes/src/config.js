8a3bef250697c4ae6461c735b255d017c7d80002
Remi Gau <remi_gau@hotmail.com>
2021-01-14 21:29:18 +0100

    update protocol and activities

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/user_name/repo_name/branch_name/protocols/schema_file_name.jsonld',
-  banner: 'This protocol is under construction.',
+  githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/reexecute_nimg_checklist/master/protocols/reexec_nimg/reexec_nimg_schema',
+  banner: 'neuroimaging re-executability checklist',
   startButton: 'Start',
-  assetsPublicPath: '/reproschema_template/',
+  assetsPublicPath: '/reexecute_nimg_checklist/',
   backendServer: 'null'
 }
