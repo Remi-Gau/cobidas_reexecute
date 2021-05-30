9b39a96a21b8677752f438666527998d63542612
Remi Gau <remi_gau@hotmail.com>
2021-05-30 22:52:22 +0200

    transfer jsonld to eCOBIDAS repo

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/reexecute_nimg_checklist/master/protocols/reexec_nimg/reexec_nimg_schema',
-  banner: 'neuroimaging re-executability checklist',
-  startButton: 'Start',
-  assetsPublicPath: '/reexecute_nimg_checklist/',
-  backendServer: 'null'
-}
+  githubSrc:
+    "https://raw.githubusercontent.com/ohbm/eCOBIDAS/master/protocols/reexec_nimg/reexec_nimg_schema",
+  banner: "neuroimaging re-executability checklist",
+  startButton: "Start",
+  assetsPublicPath: "/cobidas_reexecute/",
+  backendServer: "null",
+};
